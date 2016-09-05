55b49bb2b45e805a111903e81989f42853fc04c4
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-05 05:26:38 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class FiguresController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+end
